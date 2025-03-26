3d8c723edfb9f770ce023a81f56d15a89a877140
John Doe <surajkus@amazon.com>
2025-03-26 09:14:28 +0530

    partsUnlimited

---
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/Category.cs
@@ -0,0 +1,23 @@
+﻿//Testing
+//Testing - New
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace PartsUnlimited.Models
+{
+    public class Category
+    {
+        //My new commit
+        //Creating Pull Request
+        public int CategoryId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public List<Product> Products { get; set; }
+    }
+}
